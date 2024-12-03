abb04216f3e90a85eb282ef3e7e29e17e990df20
michele25-10 <michele.gabri01@gmail.com>
2024-12-03 11:42:08 +0100

    Creata pagina UserLoanEditDialog

--- biblioteca/src/main/java/it/unife/lp/model/Loan.java
+++ biblioteca/src/main/java/it/unife/lp/model/Loan.java
@@ -3,67 +3,133 @@ package it.unife.lp.model;
 import java.time.LocalDate;
 
 import it.unife.lp.util.IsbnUtil;
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
 
 public class Loan {
-    private String isbn;
-    private String name;
-    private String surname;
-    private LocalDate startLoan;
-    private LocalDate endLoan;
+    private final StringProperty isbn;
+    private final StringProperty name;
+    private final StringProperty surname;
+    private final StringProperty title;
+    private final ObjectProperty<LocalDate> startLoan;
+    private final ObjectProperty<LocalDate> endLoan;
+    private final BooleanProperty finished; // Nuova proprietà Boolean
 
     // Costruttore predefinito (obbligatorio per Jackson)
     public Loan() {
+        this.isbn = new SimpleStringProperty();
+        this.name = new SimpleStringProperty();
+        this.surname = new SimpleStringProperty();
+        this.title = new SimpleStringProperty();
+        this.startLoan = new SimpleObjectProperty<>();
+        this.endLoan = new SimpleObjectProperty<>();
+        this.finished = new SimpleBooleanProperty(false); // Inizializzazione a false
     }
 
-    public Loan(String isbn, String name, String surname, LocalDate startLoan, LocalDate endLoan)
+    // Costruttore con tutti i parametri
+    public Loan(String isbn, String name, String surname, String title, LocalDate startLoan, LocalDate endLoan,
+            boolean finished)
             throws IllegalArgumentException {
         if (!IsbnUtil.isValidISBN(isbn)) {
-            throw new IllegalArgumentException("Codice ISBN non è valido" + isbn);
+            throw new IllegalArgumentException("Codice ISBN non è valido: " + isbn);
         }
-        this.isbn = isbn;
-        this.name = name;
-        this.surname = surname;
-        this.startLoan = startLoan;
-        this.endLoan = endLoan;
+        this.isbn = new SimpleStringProperty(isbn);
+        this.name = new SimpleStringProperty(name);
+        this.surname = new SimpleStringProperty(surname);
+        this.title = new SimpleStringProperty(title);
+        this.startLoan = new SimpleObjectProperty<>(startLoan);
+        this.endLoan = new SimpleObjectProperty<>(endLoan);
+        this.finished = new SimpleBooleanProperty(finished); // Assegnazione del valore del parametro finished
     }
 
+    // Getter per gli attributi
     public String getIsbn() {
-        return isbn;
+        return isbn.get();
     }
 
     public String getName() {
-        return name;
+        return name.get();
     }
 
     public String getSurname() {
-        return surname;
+        return surname.get();
+    }
+
+    public String getTitle() {
+        return title.get();
     }
 
     public LocalDate getStartLoan() {
-        return startLoan;
+        return startLoan.get();
     }
 
     public LocalDate getEndLoan() {
-        return endLoan;
+        return endLoan.get();
     }
 
+    public boolean isFinished() {
+        return finished.get(); // Getter per la proprietà finished
+    }
+
+    // Setter per gli attributi
     public void setIsbn(String isbn) {
-        this.isbn = isbn;
+        this.isbn.set(isbn);
     }
 
     public void setName(String name) {
-        this.name = name;
+        this.name.set(name);
     }
 
     public void setSurname(String surname) {
-        this.surname = surname;
+        this.surname.set(surname);
+    }
+
+    public void setTitle(String title) {
+        this.title.set(title);
     }
 
     public void setStartLoan(LocalDate startLoan) {
-        this.startLoan = startLoan;
+        this.startLoan.set(startLoan);
     }
 
     public void setEndLoan(LocalDate endLoan) {
-        this.endLoan = endLoan;
+        this.endLoan.set(endLoan);
+    }
+
+    public void setFinished(boolean finished) {
+        this.finished.set(finished); // Setter per la proprietà finished
+    }
+
+    // Properties per il binding con la UI
+    public StringProperty isbnProperty() {
+        return isbn;
+    }
+
+    public StringProperty nameProperty() {
+        return name;
+    }
+
+    public StringProperty surnameProperty() {
+        return surname;
+    }
+
+    public StringProperty titleProperty() {
+        return title;
+    }
+
+    public ObjectProperty<LocalDate> startLoanProperty() {
+        return startLoan;
+    }
+
+    public ObjectProperty<LocalDate> endLoanProperty() {
+        return endLoan;
+    }
+
+    public BooleanProperty finishedProperty() {
+        return finished; // Metodo per il binding della proprietà finished
     }
 }
---
+++ biblioteca/src/main/java/it/unife/lp/view/UserLoanEditDialogController.java
@@ -0,0 +1,128 @@
+package it.unife.lp.view;
+
+import java.time.LocalDate;
+
+import it.unife.lp.model.Loan;
+import it.unife.lp.model.User;
+import it.unife.lp.util.DateUtil;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+public class UserLoanEditDialogController {
+    @FXML
+    private TextField endLoanDate;
+    @FXML
+    private TextField startLoanDate;
+    @FXML
+    private ComboBox<String> isbnTitleComboBox;
+
+    private Stage dialogStage;
+    private Loan loan;
+    private User user;
+    private boolean okClicked = false;
+
+    /**
+     * Initializesthe controller class. This method is automatically called
+     * after the fxml file has been loaded.
+     */
+    @FXML
+    private void initialize() {
+    }
+
+    /**
+     * Sets the stage of this dialog.
+     *
+     * @param dialogStage
+     */
+    public void setDialogStage(Stage dialogStage) {
+        this.dialogStage = dialogStage;
+    }
+
+    /**
+     * Sets the person to be edited in the dialog.
+     *
+     * @param user
+     */
+    public void setLoan(Loan loan, User user) {
+        this.loan = loan;
+        this.user = user;
+        startLoanDate.setText(DateUtil.format(loan.getStartLoan()));
+        endLoanDate.setText(DateUtil.format(loan.getEndLoan()));
+        isbnTitleComboBox.setValue(loan.getIsbn() + " -> " + loan.getTitle());
+    }
+
+    /**
+     * Returnstrue if the user clicked OK, false otherwise.
+     *
+     * @return
+     */
+    public boolean isOkClicked() {
+        return okClicked;
+    }
+
+    /**
+     * Called when the user clicks ok.
+     */
+    @FXML
+    private void handleOk() {
+        if (isInputValid()) {
+            String[] selctedIsbnTitleArr = isbnTitleComboBox.getValue().split(" -> ");
+
+            try {
+                loan.setIsbn(selctedIsbnTitleArr[0]);
+                loan.setTitle(selctedIsbnTitleArr[1]);
+                loan.setName(user.getName());
+                loan.setSurname(user.getSurname());
+                loan.setEndLoan(DateUtil.parse(endLoanDate.getText()));
+                loan.setStartLoan(DateUtil.parse(startLoanDate.getText()));
+            } catch (Exception e) {
+                System.err.println(e.getMessage());
+            }
+
+            okClicked = true;
+            dialogStage.close();
+        }
+    }
+
+    /**
+     * Called when the user clicks cancel.
+     */
+    @FXML
+    private void handleCancel() {
+        dialogStage.close();
+    }
+
+    /**
+     * Validates the user input in the text fields.
+     *
+     * @return true if the input is valid
+     */
+    private boolean isInputValid() {
+        String errorMessage = "";
+        if (!DateUtil.validDate(startLoanDate.getText())) {
+            errorMessage += "Data di inizio prestito non valida!\n";
+        }
+        if (!DateUtil.validDate(endLoanDate.getText())) {
+            errorMessage += "Data di fine prestito non valida!\n";
+        }
+        if (isbnTitleComboBox.getValue().isEmpty()) {
+            errorMessage += "ISBN e titolo non valido!\n";
+        }
+        if (errorMessage.length() == 0) {
+            return true;
+        } else {
+            // Show the error message.
+            Alert alert = new Alert(AlertType.ERROR);
+            alert.initOwner(dialogStage);
+            alert.setTitle("Errore di validazione");
+            alert.setHeaderText("Per piacere ricontrolla i campi dati");
+            alert.setContentText(errorMessage);
+            alert.showAndWait();
+            return false;
+        }
+    }
+}
